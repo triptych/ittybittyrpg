a9c434092252a998b78e5d877e6abef5bd354610
Andrew Wooldridge <triptych+c9@gmail.com>
2018-06-12 07:56:08 +0000

    Getting canvas to show, and title of room to display!

--- src/scripts/modules/player/html.js
+++ src/scripts/modules/player/html.js
@@ -3,8 +3,10 @@ export function buildCode(obj){
 <script src='ibrpg.js'></script>
 <script src='core.js'></script> 
 <div>
-  <h1>${obj.world.name}</h1>  
-    [Game world here]
+    <h1>${obj.world.name}</h1>  
+    <div class="game">
+        <canvas id="ibrpg"></canvas>
+    </div>
 </div>
 `;
 }
