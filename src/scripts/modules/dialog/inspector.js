0bb420b11a56a45e7fc765488e4726adb5ef8dc3
Andrew Wooldridge <awooldridge@ebay.com>
2018-06-21 15:03:18 -0700

    getting dialog text to save

--- src/scripts/modules/dialog/inspector.js
+++ src/scripts/modules/dialog/inspector.js
@@ -2,8 +2,10 @@
  * ibrpg dialog inspector -- to embed in the editor module 
  */ 
 var game = {
-    obj:null
+    obj:{}
 };
+var store = {};
+
 var data = {
     name: "default",
     characters: [
@@ -19,15 +21,15 @@ var editDialogWindow = `
         <div class="close" id='pop-close'>X</div>
         <h2>Dialog text</h2>
         <textarea class="dialog-data">
-        ${data.text}
         </textarea>
-        <div><button class="dialog-save">Save</button></div>
+        <div><button class="dialog-save" data-room="">Save</button></div>
     </div>
 `;
 
 var init = function (){
     console.log ("dialog: module init called.");
     console.log ("data:", data);
+    console.log ("store:", store);
     
 }
 
@@ -41,7 +43,7 @@ var listeners = function (evt) {
                 break;
             case 'dialog-save':
                 console.log("clicked save on popup");
-                updateValues(); 
+                updateValues(evt.target.dataset.room); 
                 break;   
             default:
                 console.log("dunno what you clicked in the popup")
@@ -63,32 +65,59 @@ var showProperties = function (obj){
     //document.body.appendChild(editDialogWindow);
     let popupwin = document.createRange().createContextualFragment(editDialogWindow);
     document.body.appendChild(popupwin);
-    
+    bindEvents();
+
     game.obj = obj;
-    if(game.obj.ibrpg.cy.elements("#"+game.obj.target.dataset['room']).data('dialog')){
-        data.text = game.obj.ibrpg.cy.elements("#"+game.obj.target.dataset['room']).data('dialog').text;
-        console.log(" --- data.text -- ",data.text);
-    } else {
-        data.text = "some default text";
-    }
+    // if(game.obj.ibrpg.cy.elements("#"+game.obj.target.dataset['room']).data('dialog')){
+    //     data.text = game.obj.ibrpg.cy.elements("#"+game.obj.target.dataset['room']).data('dialog').text;
+    //     console.log(" --- data.text -- ",data.text);
+    // } else {
+    //     data.text = "some default text";
+    // }
     // if(data.text === ){
     //     data.text = game.obj.ibrpg.cy.elements("#"+game.obj.target.dataset['room']).data('dialog').text;
 
     // }
-    document.querySelector('.popup .dialog-data').value = data.text;
+    //document.querySelector('.popup .dialog-data').value = data.text;
 
-    bindEvents();
+    var key = obj.target.dataset['room'];
+    if (!store[key]) {
+        store[key] = {
+            id: key,
+            text: "placeholder text"
+        }
+    }
+    console.log("dialog: store",store);
+    var cyText = game.obj.ibrpg.cy.$('#'+key).data("dialog");
+    console.log("dialog: cyText:", cyText);
+    if( cyText != undefined    ){
+        store[key].text = game.obj.ibrpg.cy.$('#'+key).data("dialog");
+    }
+
+    document.querySelector('.popup .dialog-data').value = store[key].text;
+    document.querySelector('.popup .dialog-save').dataset.room = key;
 }
 
-var updateValues = function () {
-    console.log("dialog: update values");
-    console.log("dialog: game.obj", game.obj);
-    console.log("dialog: game.obj.target.dataset", game.obj.target.dataset);
-    data.text = document.querySelector('.popup .dialog-data').value;
-    console.log("data.text:" ,data.text);
-    console.log("selector:", "#"+game.obj.target.dataset['room'] );
+var updateValues = function (room) {
+    // console.log("dialog: update values");
+    // console.log("dialog: game.obj", game.obj);
+    // console.log("dialog: game.obj.target.dataset", game.obj.target.dataset);
+    // data.text = document.querySelector('.popup .dialog-data').value;
+    // console.log("data.text:" ,data.text);
+    // console.log("selector:", "#"+game.obj.target.dataset['room'] );
+    // var newData = Object.assign({},data);
+    // game.obj.ibrpg.cy.$("#"+game.obj.target.dataset['room']).data('dialog',newData);
+    // console.log("objects to update",game.obj.ibrpg.cy.$("#"+game.obj.target.dataset['room']).length)
 
-    game.obj.ibrpg.cy.$("#"+game.obj.target.dataset['room']).data('dialog',data);
-}
+    console.log("dialog: updateValues called");
+    console.log("dialog: room", room);
+    //console.log("dialog: data.text before:", data.text);
+    //data.text = document.querySelector('.popup .dialog-data').value;
+    //console.log("dialog: data.text after:", data.text);
+    store[room].text = document.querySelector('.popup .dialog-data').value;
+    console.log("dialog: store[room].text", store[room].text);
+    console.log("dialog: game.obj.ibrpg.cy.$('#'+room)" , game.obj.ibrpg.cy.$('#'+room))
+    game.obj.ibrpg.cy.$('#'+room).data("dialog", store[room].text);
+};
 
 export default {init, showProperties, updateValues}
