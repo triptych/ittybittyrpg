ccecc0a139a40529ac6df307948e635e37dae1b4
Andrew Wooldridge <awooldridge@ebay.com>
2018-06-23 20:16:02 -0700

    getting new startup panel working

---
+++ src/scripts/editor.js
@@ -0,0 +1,13 @@
+/* IttyBittyRPG Editor v 0.02 */
+/* global cytoscape */
+/* global JSZip */
+'use strict';
+
+/* world constants */
+const Constants = {
+    WORLD: 'world',
+    ROOM: 'room'
+}
+
+
+
