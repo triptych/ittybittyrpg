0627ea25ea6eed36dbc2ff3e47f5696a134204c5
Andrew Wooldridge <awooldridge@ebay.com>
2019-02-03 15:44:55 -0800

    First commit

---
+++ godot/splash.gd
@@ -0,0 +1,25 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_ButtonNew_pressed():
+	print("Button New Pressed")
+
+
+func _on_ButtonOpen_pressed():
+	print("Button Open Pressed")
+
+
+func _on_ButtonQuit_pressed():
+	print("Button Quit Pressed")
